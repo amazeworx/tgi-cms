aa12751b007d355ac2bf33423099ec69d5becf76
Fransnico Susanto <fransnico@amazeworx.com>
2023-03-10 15:51:16 +0700

    update pages

--- themes/blocks/assets/js/app.js
+++ themes/blocks/assets/js/app.js
@@ -1,2 +1,21 @@
 (() => {
+  // resources/js/app.js
+  jQuery(function($) {
+    var shrinkHeader = 100;
+    headerShrink();
+    $(window).scroll(function() {
+      headerShrink();
+    });
+    function getCurrentScroll() {
+      return window.pageYOffset || document.documentElement.scrollTop;
+    }
+    function headerShrink() {
+      var scroll = getCurrentScroll();
+      if (scroll >= shrinkHeader) {
+        $(".header").addClass("header-shrink");
+      } else {
+        $(".header").removeClass("header-shrink");
+      }
+    }
+  });
 })();
--- themes/blocks/resources/js/app.js
+++ themes/blocks/resources/js/app.js
@@ -1,3 +1,24 @@
 // import '../css/app.css';
 
 // console.log('test')
+
+jQuery(function ($) {
+  //the shrinkHeader variable is where you tell the scroll effect to start.
+  var shrinkHeader = 100;
+  headerShrink();
+  $(window).scroll(function () {
+    //console.log(scroll);
+    headerShrink();
+  });
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
+  function headerShrink() {
+    var scroll = getCurrentScroll();
+    if (scroll >= shrinkHeader) {
+      $(".header").addClass("header-shrink");
+    } else {
+      $(".header").removeClass("header-shrink");
+    }
+  }
+});
